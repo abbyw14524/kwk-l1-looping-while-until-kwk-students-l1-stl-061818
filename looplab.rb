071596238389c9d1033a2aacf6950cbaa9b907b8
abbyw14524 <awhalen111@rsdmo.org>
2018-06-19 19:18:02 +0000

    automatically backed up by learn

---
+++ looplab.rb
@@ -0,0 +1,18 @@
+def cupid_shuffle
+  counter = 6 
+  
+  while counter < 10 
+    puts "Wingardium Leviosa"
+    puts ""
+    
+  counter = counter + 1 
+  end
+  
+end
+
+cupid_shuffle
+
+
+
+def cupid_shuffle
+  counter = 0
